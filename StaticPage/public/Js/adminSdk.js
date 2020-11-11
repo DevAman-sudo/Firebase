8992e29cabe9e3db85aeb63230954ab45aa7ac07
DevAman-sudo <devaman.net@gmail.com>
2020-11-11 18:35:10 +0545

    Task Manager

---
+++ StaticPage/public/Js/adminSdk.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyDl5bQxK07DTXDe2iaZ-2dOFcD4vyHivic",
+    authDomain: "task-manager-236a1.firebaseapp.com",
+    databaseURL: "https://task-manager-236a1.firebaseio.com",
+    projectId: "task-manager-236a1",
+    storageBucket: "task-manager-236a1.appspot.com",
+    messagingSenderId: "641010616604",
+    appId: "1:641010616604:web:fa666071b08a62ab90d2d9",
+    measurementId: "G-6C4YMD6R93"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
