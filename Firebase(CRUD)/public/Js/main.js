258397aa57528cd75c29b2f6a13cdf05425c4c9f
DevAman-sudo <devaman.net@gmail.com>
2020-11-13 07:22:19 +0545

    Task Manager

--- Firebase(CRUD)/public/Js/main.js
+++ Firebase(CRUD)/public/Js/main.js
@@ -1,5 +1,6 @@
 const formContainer = document.querySelector('.data-store');
 const input = document.getElementById('text');
+const button = document.querySelector('.button');
 // firebase realtime database access //
 const database = firebase.database();
 
@@ -10,7 +11,7 @@ database.ref('/CRUD/crud/').set({
 
 // update data //
 database.ref('/CRUD/crud/').update({
-    data: "hello hello world" 
+    data: "hello hello world"
 });
 
 // read data //
@@ -22,4 +23,39 @@ database.ref('/CRUD/crud/').on('value', (snapshot) => {
         h2.innerText = data;
         formContainer.appendChild(h2);
     }
+});
+
+// delete data //
+// database.ref('/CRUD/crud').on((snapshot) => {
+// snapshot.forEach((childSnapshot) => {
+// snapshot.remove();
+// });
+// });
+
+// write function on click //
+function write() {
+    database.ref('/USER/users/').push({
+        data: input.value
+    });
+}
+
+// read data call back function //
+database.ref('/USER/users/').on('value', (snapshot) => {
+    let data ;
+    snapshot.forEach( (childSnapshot) => {
+        data = childSnapshot.val();
+    });
+    for (let key in data) {
+        data = data[key];
+        let h2 = document.createElement('h2');
+        h2.innerText = data;
+        formContainer.appendChild(h2);
+    }
+});
+
+// listening event of form button
+button.addEventListener('click', (event) => {
+    // prevent Form from sumbitting
+    event.preventDefault();
+    write();
 });
