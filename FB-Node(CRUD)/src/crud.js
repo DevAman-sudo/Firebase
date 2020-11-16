80ca3df40756a5fe57e0e586ab5cbc3a23f2b487
DevAman-sudo <devaman.net@gmail.com>
2020-11-16 08:30:22 +0545

    Task Manager

--- FB-Node(CRUD)/src/crud.js
+++ FB-Node(CRUD)/src/crud.js
@@ -2,23 +2,52 @@ const express = require('express');
 const chalk = require('chalk');
 const path = require('path');
 const admin = require('firebase-admin');
+var uniqueKey = require('unique-keygen');
 const app = express();
-const port = process.env.PORT || 8080 ;
+const port = process.env.PORT || 8080;
 
 // firebase-admin service account //
-const serviceAccount = dbDataPath;
+const serviceAccount = path.join(__dirname, '../db.json');
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://task-manager-236a1.firebaseio.com"
 });
+// unique id fetch //
+let unique = uniqueKey(20);
+//
 const database = admin.database();
-const ref = database.ref('/NODE/CRUD/');
 
 // CRUD Operations .. //
 // CREATE DATA //
+function create(user_id , username , email , passkey) {
+    database.ref(`/Node/Crud/`).push({
+        user_id: user_id,
+        username: username,
+        email: email,
+        passkey: passkey,
+    });
+}
+// create(unique, 'DevAman', 'DevAman@gmail.com', 'admin');
+
+// READ DATA //
+function read() {
+    database.ref('/Node/Crud/').on('value', (snapshot) => {
+        // console.log(snapshot);
+        snapshot.forEach((childSnapshot) => {
+            let data = childSnapshot.val();
+            let key = childSnapshot.key;
+            console.log(key);
+            console.log(data);
+        });
+    });
+}
+read();
 
 // listining on port 8080 //
-app.listen( port , (err , data) => {
-    if (err) { console.log( chalk.blue.bgRed.bold(`Error Found = ${err}`) ) }
-    else { console.log( chalk.red.bgBlue.bold(`http://127.0.0.1:8080`) ) }
+app.listen(port, (err, data) => {
+    if (err) {
+        console.log(chalk.blue.bgRed.bold(`Error Found = ${err}`));
+    } else {
+        console.log(chalk.red.bgBlue.bold(`http://127.0.0.1:8080`));
+    }
 });
