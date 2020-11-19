02bd182681664ce54f9d95d22a49d6dd735eca29
DevAman-sudo <devaman.net@gmail.com>
2020-11-19 15:38:53 +0545

    firebase auth setup

---
+++ FB-Node(AUTH)/src/crud.js
@@ -0,0 +1,35 @@
+// Npm Packages //
+const express = require('express');
+const chalk = require('chalk');
+const path = require('path');
+const hbs = require('hbs');
+var admin = require("firebase-admin");
+
+// App and Port Setup //
+const app = express();
+const port = process.env.PORT || 8080 ;
+
+// File Path Decleration //
+const staticPath = path.join( __dirname , '../public');
+
+// using public folder to serve //
+app.use(express.static(staticPath));
+
+// app routing //
+// main page //
+app.get('/' , (req , res) => {
+    res.sendFile('index.html');
+});
+// 404 Error Page //
+app.get('*' , (req , res) => {
+    res.sendFile('404.html');
+});
+
+// listining on port 8080 //
+app.listen( port , (err) => {
+    if (err) {
+        console.log( chalk.blue.bgRed.bold(`Error Found: ${err}`) );
+    } else {
+        console.log( chalk.red.bgBlue.bold(`http://127.0.0.1:${port}`) );
+    }
+});
