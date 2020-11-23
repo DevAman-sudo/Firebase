5f635b78fb5bb994bcdab25ff457601065673d01
DevAman-sudo <devaman.net@gmail.com>
2020-11-23 18:48:37 +0545

    client side socket io connection

--- FirebaseAuth/public/Js/index.js
+++ FirebaseAuth/public/Js/index.js
@@ -1,8 +1,13 @@
 // javascript DOM property of index.hbs 
 const button = document.getElementById('button');
+const body = document.querySelector('.main-container');
 
+// Socket connection from clint to server //
+const socket = io('http://localhost');
+
+// Clicked function for click event //
+function clicked() {
+    body.style.background = "red" ;
+}
 // Click event on button 
-button.addEventListener('click' , (event) => {
-    event.preventDeafult();
-    button.href = '/LogIn' ;
-});
+button.addEventListener('click' , clicked);
--- FirebaseAuth/src/index.js
+++ FirebaseAuth/src/index.js
@@ -4,16 +4,17 @@ const chalk = require('chalk');
 const path = require('path');
 const EventEmitter = require('events');
 const io = require('socket.io');
+const socket = require('socket.io-client')('http://127.0.0.1:8080');
 const fs = require('fs');
 const admin = require("firebase-admin");
 const event = new EventEmitter();
 
 // App and port setup //
 const app = express();
-const port = process.env.PORT || 8080 ;
+const port = process.env.PORT || 8080;
 
 // file path declaration //
-const staticPath = path.join( __dirname , '../public');
+const staticPath = path.join(__dirname, '../public');
 
 // Setup view engine as hbs //
 app.set('view engine', 'hbs');
@@ -23,35 +24,37 @@ app.use(express.static(staticPath));
 
 // app routing //
 // main router //
-app.get('/' , (req , res) => {
+app.get('/', (req, res) => {
     res.render('index');
     // Event on button click //
-    event.on('goToSingUp' , () => {
-        // console.log('hell9 world');
+    socket.on('hello' , () => {
+        console.log('hello world');
     });
-    event.emit('goToSingUp');
 });
+// event.on('hello', () => {
+    // console.log('hell9 world');
+// });
 
-// SignUp Page // 
-app.get('/signup' , (req , res) => {
+// SignUp Page //
+app.get('/signup', (req, res) => {
     res.render('SignUp');
 });
 
 // LogIn Page //
-app.get('/login' , (req , res) => {
+app.get('/login', (req, res) => {
     res.render('login');
 });
 
 // 404 Error page Routing //
-app.get('*' , (req , res) => {
+app.get('*', (req, res) => {
     res.render('404');
 });
 
 // listning on port 8080 //
-app.listen( port , (err) => {
+app.listen(port, (err) => {
     if (err) {
-        console.log( chalk.blue.bgRed.bold(`Error Found: ${err}`) );
+        console.log(chalk.blue.bgRed.bold(`Error Found: ${err}`));
     } else {
-        console.log( chalk.red.bgBlue.bold(`http://127.0.0.1:${port}`) );
+        console.log(chalk.red.bgBlue.bold(`http://127.0.0.1:${port}`));
     }
 });
